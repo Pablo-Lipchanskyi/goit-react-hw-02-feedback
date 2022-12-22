8230ea2dd7294cd9f5829a1d72f1894de6d89ae1
Pablo-Lipchanskyi <pablolipchansky@gmail.com>
2022-12-22 12:06:10 +0200

    counter

--- src/components/FeedbackCounter.jsx
+++ src/components/FeedbackCounter.jsx
@@ -1,12 +1,51 @@
-import React, { Component } from "react";
+import React from "react";
+import PropTypes from 'prop-types';
 
-class FeedbackCounter extends Component{
-    constructor() {
-        super()
-        this.state = {
+export class FeedbackCounter extends React.Component{
+    static propTypes = {
+        good: PropTypes.number
+    }
+        state = {
             good: 0,
             neutral: 0,
             bad: 0
-        }
     }
+    handleIncrement = () => {
+        this.setState((prevState) => {
+            return {
+                good: prevState.good.value + 1
+            }
+        }
+        
+       
+    )
 }
+
+    render() {
+        return (
+            <div>
+                <h2>Please leave feedback</h2>
+                <ul>
+                    <li>
+                        <button onClick={this.handleIncrement} type="button">Good
+                        </button>
+                    </li>
+                    <li>
+                        <button onClick={()=>{console.log("rkbr")}} type="button">Neutral
+                        </button>
+                    </li>
+                    <li>
+                        <button onClick={()=>{console.log("rkbr")}} type="button">Bad
+                        </button>
+                    </li>
+                </ul>
+                <h2>Statistics</h2>
+                <ul>
+                    <li>Good:{this.state.good}</li>
+                    <li>Neutral:{this.state.neutral}</li>
+                    <li>Bad:{this.state.bad}</li>
+                </ul>
+            </div>
+        )
+    }
+    }
