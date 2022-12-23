5537f03ca99cf9a8952926a6ea17ced94bcf2d50
Pablo-Lipchanskyi <pablolipchansky@gmail.com>
2022-12-23 10:46:18 +0200

    feedback

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,8 +1,51 @@
-import { FeedbackCounter } from "./FeedbackCounter";
-export const App = () => {
-  return (
-    <div>
-      <FeedbackCounter/>
-   </div>
-  );
-};
+import React from 'react';
+import { Section } from 'components/Section/Section';
+import { Statistics } from 'components/Statistics/Statistics';
+import { FeedbackOptions } from 'components/FeedbackOptions/FeedbackOptions';
+import css from 'components/app.module.css'
+export class App extends React.Component {
+  state = {
+    good: 0,
+    neutral: 0,
+    bad: 0,
+  };
+
+  valueIncrement = option => {
+    this.setState(prevState => ({
+      [option]: prevState[option] + 1,
+    }));
+  };
+
+  countTotalFeedback = () => {
+    return this.state.good + this.state.neutral + this.state.bad;
+  };
+
+  countPositiveFeedbackPercentage = () => {
+    return Math.round((this.state.good / this.countTotalFeedback()) * 100);
+  };
+
+  render() {
+    const { good, neutral, bad } = this.state;
+    const options = Object.keys(this.state);
+    return (
+      <div className={css.div}>
+        <Section title="Please leave feedback">
+          <FeedbackOptions
+            options={options}
+            onLeaveFeedback={this.valueIncrement}
+          />
+        </Section>
+
+        <Section title="Statistics">
+          <Statistics
+            good={good}
+            neutral={neutral}
+            bad={bad}
+            total={this.countTotalFeedback()}
+            positivePercentage={this.countPositiveFeedbackPercentage() || 0}
+          />
+        </Section>
+        </div>
+    );
+  }
+}
